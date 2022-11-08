6ded9924db85efb1c1d2fe85a01e6abcc8ac48ad
Jenniffer Melchiade <jdenisemelch@gmail.com>
2022-11-08 09:26:45 -0500

    created database and todo table, created a todo, get a todo, and get all todos

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//routes
+// create a todo
+
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING * ",
+      [description]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// get a todo
+
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// update a todo
+// delete a todo
 
 app.listen(3000, () => {
   console.log("server listening on port 3000");
